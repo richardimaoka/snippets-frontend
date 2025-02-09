38325f5971fddb88e317bad427409964a7db9d83
richardimaoka <richard.s.imaoka@gmail.com>
2025-02-09 12:35:49 +0900

    horizontal 2-item cards

--- nextjs/app/components/card/horizontal-parts/2-items/1fr-1fr/Component.tsx
+++ nextjs/app/components/card/horizontal-parts/2-items/1fr-1fr/Component.tsx
@@ -7,9 +7,7 @@ type Props = {
 export function Component(props: Props) {
   return (
     <div className={styles.component}>
-      <div
-        style={{ height: "24px", width: "24px", backgroundColor: "#585858" }}
-      ></div>
+      <div style={{ height: "24px", backgroundColor: "#585858" }}></div>
       <div className={styles.text}>{props.text}</div>
     </div>
   );
--- nextjs/app/components/card/horizontal-parts/2-items/1fr-auto/Component.tsx
+++ nextjs/app/components/card/horizontal-parts/2-items/1fr-auto/Component.tsx
@@ -1,10 +1,6 @@
 import styles from "./Component.module.css";
-import Image from "next/image";
 
 type Props = {
-  imageSrc: string;
-  imageWidth: number;
-  imageHeight: number;
   text: string;
 };
 
@@ -12,13 +8,9 @@ export function Component(props: Props) {
   return (
     <div className={styles.component}>
       <div className={styles.text}>{props.text}</div>
-      <Image
-        className={styles.image}
-        src={props.imageSrc}
-        width={props.imageWidth}
-        height={props.imageHeight}
-        alt={props.text}
-      />
+      <div
+        style={{ height: "24px", width: "24px", backgroundColor: "#585858" }}
+      ></div>
     </div>
   );
 }
